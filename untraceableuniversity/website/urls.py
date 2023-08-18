8cf7c9daf1753fd1bde5a0435a6aed12c92f9e23
Paul Hoekman <paulhoekman@untraceable-university.org>
2023-08-18 09:10:04 +0200

    Join and mission pages

--- untraceableuniversity/website/urls.py
+++ untraceableuniversity/website/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("join/<slug:slug>/", views.join_us),
+    path("join/<slug:slug>/", views.join_us, name="join_us"),
+    path("join/", views.join_us, name="join_us"),
+    path("mission/", views.page, { "slug": "mission" }, name="mission"),
 ]
--- untraceableuniversity/website/views.py
+++ untraceableuniversity/website/views.py
@@ -8,15 +8,27 @@ def get_page(slug):
 @csrf_exempt
 def index(request):
     context = {
+        "menu_transparent": True,
+        "menu": "home",
     }
 
     return render(request, "index.html", context)
 
 @csrf_exempt
-def join_us(request, slug):
+def join_us(request, slug="join"):
     context = {
         "info": get_page(slug),
+        "menu": "join",
     }
 
     return render(request, "join.html", context)
+
+@csrf_exempt
+def page(request, slug):
+    context = {
+        "info": get_page(slug),
+        "menu": slug,
+    }
+
+    return render(request, "page.html", context)
 
